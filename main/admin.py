5b31799dcee1763f2f0ddc6c0dcc1eb2c5534c3f
EtaTauri <pleiades.ahn@gmail.com>
2018-10-03 18:53:32 +0900

    alpha, first commit

---
+++ main/admin.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
+
+from .models import Article
+
+# Register your models here.
+admin.site.register(Article)
